826094a838d6caec86f2952665b7f91d927fb898
AnnonyWOLFRODA <annonywolfroda@gmail.com>
2025-08-23 13:33:34 +0000

    fix: use of venv

--- admin-panel/init.sh
+++ admin-panel/init.sh
@@ -5,11 +5,6 @@ echo "[+] Installing system dependencies"
 sudo apt update
 sudo apt install -y python3.12 python3-venv python3-pip python3-dev python-is-python3
 
-echo "[+] Installing web browsing packages"
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install -y ./google-chrome-stable_current_amd64.deb
-sudo rm -rf ./google-chrome-stable_current_amd64.deb
-
 echo "[+] Creating virtual environment"
 python3 -m venv venv
 
@@ -24,4 +19,4 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "[✔] Setup complete. Virtual environment is active."
-echo "[i] To use the bot, run 'source venv/bin/activate' and then 'python3 src/main.py'."
+echo "[i] To use the bot, run 'source venv/bin/activate' and then 'python3 app.py'."
